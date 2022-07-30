157aa654dca5d03a502249251042bffce4148f99
delalo <bernardahlijah3@gmail.com>
2022-07-30 13:02:25 +0000

    Home page of the ANIDASOC PA HERBAL Project

--- website/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from dashboard.models import Order
-
-class CheckoutForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ['ordered_by', 'shipping_address', 'mobile', 'email']
