732b57c0a6aea581843edab55e3a4c8c77401cd1
Mustapha Adams <adamsmustapha419@gmail.com>
2022-08-04 12:48:06 +0000

    profile model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,5 @@
+from distutils.command.upload import upload
+from turtle import up
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,5 +10,12 @@ class Customer(models.Model):
     address = models.CharField(max_length=200, null=True, blank=True)
     joined_on = models.DateTimeField(auto_now_add=True)
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    profile_image = models.ImageField(upload_to='images/profile')
+
     def __str__(self):
-        return self.full_name
+        return self.first_name
