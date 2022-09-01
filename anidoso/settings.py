b056e4f65ce01a1e6414a5bb6eb95fffa9046d32
BINAH LOUIS <visteen192@gmail.com>
2022-09-01 17:10:21 +0000

    louis

--- anidoso/settings.py
+++ anidoso/settings.py
@@ -133,7 +133,7 @@ import os
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
